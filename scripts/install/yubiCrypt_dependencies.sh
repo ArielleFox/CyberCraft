4970df50a15c091d287de778be6fb55fd0a42c24
Arielle Fox <118761163+ArielleFox@users.noreply.github.com>
2025-04-23 20:59:51 +0200

    Update yubiCrypt_dependencies.sh

--- scripts/install/yubiCrypt_dependencies.sh
+++ scripts/install/yubiCrypt_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-
+if [ -d ~/yubiCrypt ]; then
+  echo "yubiCrypt Folder exists."
+else
+  cd ~/
+  git clone https://github.com/ArielleFox/yubiCrypt.git
+  cd yubiCrypt
+  ./installer.py
+fi
