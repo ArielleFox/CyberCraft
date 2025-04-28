c9563ccc702dc1b6527feb35575f4c72e67c9ff7
Arielle Fox <118761163+ArielleFox@users.noreply.github.com>
2025-04-28 18:28:46 +0200

    Updated: $VERSION

--- .commit.sh
+++ .commit.sh
@@ -4,4 +4,6 @@ VERSION=$(cat .version.txt)
 make
 rm -rf ~/CyberCraft/yubiCrypt/modules/__pycache__
 git add ~/CyberCraft/src/cybercraft
-git commit -m 'Updated: $($VERSION) Binary Build'
+git commit -m 'Updated: $VERSION Binary Build'
+git commit -a -m 'Updated: $VERSION'
+git push 
